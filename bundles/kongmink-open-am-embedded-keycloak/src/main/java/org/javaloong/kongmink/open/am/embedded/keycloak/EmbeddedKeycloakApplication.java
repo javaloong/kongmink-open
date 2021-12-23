bb70568ecd21cc7dbcaba59a6da6030b8a5f88ca
tutunannan <bd66_6@hotmail.com>
2021-12-23 19:06:09 +0800

    feat: add embedded keycloak

---
+++ bundles/kongmink-open-am-embedded-keycloak/src/main/java/org/javaloong/kongmink/open/am/embedded/keycloak/EmbeddedKeycloakApplication.java
@@ -0,0 +1,72 @@
+package org.javaloong.kongmink.open.am.embedded.keycloak;
+
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.services.managers.ApplianceBootstrap;
+import org.keycloak.services.managers.RealmManager;
+import org.keycloak.services.resources.KeycloakApplication;
+import org.keycloak.services.util.JsonConfigProviderFactory;
+import org.keycloak.util.JsonSerialization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+
+public class EmbeddedKeycloakApplication extends KeycloakApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(EmbeddedKeycloakApplication.class);
+
+    public EmbeddedKeycloakApplication() {
+        this(new KeycloakServerProperties());
+    }
+
+    public EmbeddedKeycloakApplication(KeycloakServerProperties serverProperties) {
+        super();
+        createMasterRealmAdminUser(serverProperties.getAdminUser(), serverProperties.getAdminPassword());
+        createConfigRealm(serverProperties.getRealmConfigPath());
+    }
+
+    protected void loadConfig() {
+        JsonConfigProviderFactory factory = new RegularJsonConfigProviderFactory();
+        Config.init(factory.create().orElseThrow(() -> new NoSuchElementException("No value present")));
+    }
+
+    private void createMasterRealmAdminUser(String adminUser, String adminUserPassword) {
+        KeycloakSession session = getSessionFactory().create();
+        ApplianceBootstrap applianceBootstrap = new ApplianceBootstrap(session);
+        try {
+            session.getTransactionManager().begin();
+            applianceBootstrap.createMasterRealmUser(adminUser, adminUserPassword);
+            session.getTransactionManager().commit();
+        } catch (Exception ex) {
+            log.warn("Couldn't create keycloak master admin user: {}", ex.getMessage());
+            session.getTransactionManager().rollback();
+        }
+        session.close();
+    }
+
+    private void createConfigRealm(String path) {
+        KeycloakSession session = getSessionFactory().create();
+        try {
+            session.getTransactionManager().begin();
+            RealmManager manager = new RealmManager(session);
+            InputStream stream = getClass().getClassLoader().getResourceAsStream(path);
+            if (stream == null) {
+                log.error("Unable to find keycloak realm  in file: {}", path);
+                return;
+            }
+            manager.importRealm(JsonSerialization.readValue(stream, RealmRepresentation.class));
+            session.getTransactionManager().commit();
+        } catch (Exception ex) {
+            log.warn("Failed to import Realm json file: {}", ex.getMessage());
+            session.getTransactionManager().rollback();
+        }
+        session.close();
+    }
+
+    public static class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+    }
+}
---
+++ bundles/kongmink-open-am-embedded-keycloak/src/main/java/org/javaloong/kongmink/open/am/embedded/keycloak/EmbeddedKeycloakRequestFilter.java
@@ -0,0 +1,64 @@
+package org.javaloong.kongmink.open.am.embedded.keycloak;
+
+import org.keycloak.common.ClientConnection;
+import org.keycloak.services.filters.AbstractRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class EmbeddedKeycloakRequestFilter extends AbstractRequestFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        ClientConnection clientConnection = createConnection((HttpServletRequest) servletRequest);
+
+        filter(clientConnection, (session) -> {
+            try {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private ClientConnection createConnection(HttpServletRequest request) {
+        return new ClientConnection() {
+            @Override
+            public String getRemoteAddr() {
+                return request.getRemoteAddr();
+            }
+
+            @Override
+            public String getRemoteHost() {
+                return request.getRemoteHost();
+            }
+
+            @Override
+            public int getRemotePort() {
+                return request.getRemotePort();
+            }
+
+            @Override
+            public String getLocalAddr() {
+                return request.getLocalAddr();
+            }
+
+            @Override
+            public int getLocalPort() {
+                return request.getLocalPort();
+            }
+        };
+    }
+}
