ba9f19a654cbcad09c1a69b07c775ecd6dc121b1
tutunannan <bd66_6@hotmail.com>
2022-01-21 14:17:52 +0800

    feat: add jackson object mapper resolver

---
+++ bundles/kongmink-open-rest-core/src/main/java/org/javaloong/kongmink/open/rest/core/internal/mapper/ObjectMapperResolver.java
@@ -0,0 +1,44 @@
+package org.javaloong.kongmink.open.rest.core.internal.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.javaloong.kongmink.open.rest.RESTConstants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsApplicationSelect;
+import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsExtension;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+/**
+ * Customizing ObjectMapper.
+ *
+ * @author Xu Cheng
+ */
+@Component
+@JaxrsExtension
+@JaxrsApplicationSelect("(" + JaxrsWhiteboardConstants.JAX_RS_NAME + "=" + RESTConstants.JAX_RS_NAME + ")")
+@Provider
+public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper mapper;
+
+    public ObjectMapperResolver() {
+        this.mapper = createObjectMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return mapper;
+    }
+
+    private static ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setTimeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+        return objectMapper;
+    }
+}
